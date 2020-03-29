6407b49002a1952b2c16015e3b3a23d4d32ea603
Abhay Vincent <abhaykvincent@gmail.com>
2020-03-28 20:42:21 -0400

    React Init files

---
+++ build/precache-manifest.2c0b6708eea03c1f4bcb8aaecb3b5b1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d8e2996211883f223acf6f12739b7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc486e42c0ab2f66fcc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d70a0acee425b12c921",
+    "url": "/static/js/2.a05b8663.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a05b8663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc486e42c0ab2f66fcc",
+    "url": "/static/js/main.b4302c76.chunk.js"
+  },
+  {
+    "revision": "c2583cf863f454436578",
+    "url": "/static/js/runtime-main.e77418c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
